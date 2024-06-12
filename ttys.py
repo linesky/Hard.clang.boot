6f0125e558ad1baf8d66f8e4f5c215943416f9b6
sky line <32512284+linesky@users.noreply.github.com>
2024-06-12 19:38:44 +0100

    Add files via upload

---
+++ ttys.py
@@ -0,0 +1,37 @@
+import sys
+def splitss(s:str,i:int):
+    ii=i-1
+    s=s.replace("\r","\n")
+    s=s.replace("\n\n","\n")
+    ss=s.split("\n")
+    for s1 in ss:
+        if len(s1)<i:
+            print(s1)
+        else:
+            while s1!="":
+                if len(s1)<i:
+                    print(s1)
+                    s1=""
+                else:
+                    print(s1[0:ii])
+                    s1=s1[i:]
+
+
+
+def readstdin(s:str,arg1:int):
+
+    f1=open(s,"r")
+    ss=f1.read()
+    f1.close()
+    splitss(ss,arg1)
+    
+
+    
+print("\x1b[43;37m")
+if len(sys.argv)>1:
+    readstdin(sys.argv[1],int(sys.argv[2]))
+
+
+
+
+
