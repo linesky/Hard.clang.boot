49bd6fe607385cec37cec39755e01fe04dd569e4
sky line <32512284+linesky@users.noreply.github.com>
2024-06-13 19:58:51 +0100

    Add files via upload

---
+++ rand.py
@@ -0,0 +1,16 @@
+import random
+import sys
+def readstdin():
+    print(random.randrange(0,int(sys.argv[1])))
+    
+
+    
+print("\x1b[43;37m")
+random.SystemRandom(random.random())
+if 0==0:
+    readstdin()
+
+
+
+
+
